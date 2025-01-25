08325db9325de30e214deb52bae2feb025df638f
jeffy-g <hirotom1107@gmail.com>
2025-01-26 00:41:11 +0900

    eliminate waste

--- v2/post_universe_ids_ok.d.ts
+++ v2/post_universe_ids_ok.d.ts
@@ -1,220 +1,86 @@
 /*!
  * ESI endpoint: /universe/ids/
  */
-/**
- * agents array
- *
- * @maxItems 500
- */
-type PostUniverseIdsAgents = PostUniverseIdsAgent[];
-/**
- * alliances array
- *
- * @maxItems 500
- */
-type PostUniverseIdsAlliances = PostUniverseIdsAlliance[];
-/**
- * characters array
- *
- * @maxItems 500
- */
-type PostUniverseIdsCharacters = PostUniverseIdsCharacter[];
-/**
- * constellations array
- *
- * @maxItems 500
- */
-type PostUniverseIdsConstellations = PostUniverseIdsConstellation[];
-/**
- * corporations array
- *
- * @maxItems 500
- */
-type PostUniverseIdsCorporations = PostUniverseIdsCorporation[];
-/**
- * factions array
- *
- * @maxItems 500
- */
-type PostUniverseIdsFactions = PostUniverseIdsFaction[];
-/**
- * inventory_types array
- *
- * @maxItems 500
- */
-type PostUniverseIdsInventoryTypes = PostUniverseIdsInventoryType[];
-/**
- * regions array
- *
- * @maxItems 500
- */
-type PostUniverseIdsRegions = PostUniverseIdsRegion[];
-/**
- * stations array
- *
- * @maxItems 500
- */
-type PostUniverseIdsStations = PostUniverseIdsStation[];
-/**
- * systems array
- *
- * @maxItems 500
- */
-type PostUniverseIdsSystems = PostUniverseIdsSystem[];
 
 /**
- * 200 ok object
+ * shared interface
  */
-interface PostUniverseIdsOk {
-  agents?: PostUniverseIdsAgents;
-  alliances?: PostUniverseIdsAlliances;
-  characters?: PostUniverseIdsCharacters;
-  constellations?: PostUniverseIdsConstellations;
-  corporations?: PostUniverseIdsCorporations;
-  factions?: PostUniverseIdsFactions;
-  inventory_types?: PostUniverseIdsInventoryTypes;
-  regions?: PostUniverseIdsRegions;
-  stations?: PostUniverseIdsStations;
-  systems?: PostUniverseIdsSystems;
-  [k: string]: unknown | undefined;
-}
-/**
- * agent object
- */
-interface PostUniverseIdsAgent {
+interface PostUniverseIdsEntity {
+    /**
+     * id integer
+     */
+    id?: number;
+    /**
+     * name string
+     */
+    name?: string;
+    [k: string]: unknown | undefined;
+  }
+  
   /**
-   * id integer
+   * 200 ok object
    */
-  id?: number;
-  /**
-   * name string
-   */
-  name?: string;
-  [k: string]: unknown | undefined;
-}
-/**
- * alliance object
- */
-interface PostUniverseIdsAlliance {
-  /**
-   * id integer
-   */
-  id?: number;
-  /**
-   * name string
-   */
-  name?: string;
-  [k: string]: unknown | undefined;
-}
-/**
- * character object
- */
-interface PostUniverseIdsCharacter {
-  /**
-   * id integer
-   */
-  id?: number;
-  /**
-   * name string
-   */
-  name?: string;
-  [k: string]: unknown | undefined;
-}
-/**
- * constellation object
- */
-interface PostUniverseIdsConstellation {
-  /**
-   * id integer
-   */
-  id?: number;
-  /**
-   * name string
-   */
-  name?: string;
-  [k: string]: unknown | undefined;
-}
-/**
- * corporation object
- */
-interface PostUniverseIdsCorporation {
-  /**
-   * id integer
-   */
-  id?: number;
-  /**
-   * name string
-   */
-  name?: string;
-  [k: string]: unknown | undefined;
-}
-/**
- * faction object
- */
-interface PostUniverseIdsFaction {
-  /**
-   * id integer
-   */
-  id?: number;
-  /**
-   * name string
-   */
-  name?: string;
-  [k: string]: unknown | undefined;
-}
-/**
- * inventory_type object
- */
-interface PostUniverseIdsInventoryType {
-  /**
-   * id integer
-   */
-  id?: number;
-  /**
-   * name string
-   */
-  name?: string;
-  [k: string]: unknown | undefined;
-}
-/**
- * region object
- */
-interface PostUniverseIdsRegion {
-  /**
-   * id integer
-   */
-  id?: number;
-  /**
-   * name string
-   */
-  name?: string;
-  [k: string]: unknown | undefined;
-}
-/**
- * station object
- */
-interface PostUniverseIdsStation {
-  /**
-   * id integer
-   */
-  id?: number;
-  /**
-   * name string
-   */
-  name?: string;
-  [k: string]: unknown | undefined;
-}
-/**
- * system object
- */
-interface PostUniverseIdsSystem {
-  /**
-   * id integer
-   */
-  id?: number;
-  /**
-   * name string
-   */
-  name?: string;
-  [k: string]: unknown | undefined;
-}
+  interface PostUniverseIdsOk {
+    /**
+     * agents array
+     *
+     * @maxItems 500
+     */
+    agents?: PostUniverseIdsEntity[];
+    /**
+     * alliances array
+     *
+     * @maxItems 500
+     */
+    alliances?: PostUniverseIdsEntity[];
+    /**
+     * characters array
+     *
+     * @maxItems 500
+     */
+    characters?: PostUniverseIdsEntity[];
+    /**
+     * constellations array
+     *
+     * @maxItems 500
+     */
+    constellations?: PostUniverseIdsEntity[];
+    /**
+     * corporations array
+     *
+     * @maxItems 500
+     */
+    corporations?: PostUniverseIdsEntity[];
+    /**
+     * factions array
+     *
+     * @maxItems 500
+     */
+    factions?: PostUniverseIdsEntity[];
+    /**
+     * inventory_types array
+     *
+     * @maxItems 500
+     */
+    inventory_types?: PostUniverseIdsEntity[];
+    /**
+     * regions array
+     *
+     * @maxItems 500
+     */
+    regions?: PostUniverseIdsEntity[];
+    /**
+     * stations array
+     *
+     * @maxItems 500
+     */
+    stations?: PostUniverseIdsEntity[];
+    /**
+     * systems array
+     *
+     * @maxItems 500
+     */
+    systems?: PostUniverseIdsEntity[];
+    [k: string]: unknown | undefined;
+  }
+  
