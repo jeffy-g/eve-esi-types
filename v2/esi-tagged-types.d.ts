5b2be83f855c9179ba91ebb080423c84acf3f13f
jeffy-g <hirotom1107@gmail.com>
2025-03-14 19:52:55 +0900

    v3.0.4

--- v2/esi-tagged-types.d.ts
+++ v2/esi-tagged-types.d.ts
@@ -9,7 +9,7 @@
  * THIS DTS IS AUTO GENERATED, DO NOT EDIT
  * 
  * @file eve-esi-types/v2/esi-tagged-types.d.ts
- * @summary This file is auto-generated and defines version 3.0.3 of the EVE Online ESI response types.
+ * @summary This file is auto-generated and defines version 3.0.4 of the EVE Online ESI response types.
  */
 import { TESIResponseOKMap, TPathParamsNever } from "./index.d.ts";
 export * from "./index.d.ts";
