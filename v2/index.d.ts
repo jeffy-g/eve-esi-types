c3d2ca30a2f6f09a6123314441994c8f4dc6d23d
jeffy-g <hirotom1107@gmail.com>
2025-04-19 22:32:43 +0900

    chore(release): v3.2.1

--- v2/index.d.ts
+++ v2/index.d.ts
@@ -9,7 +9,7 @@
  * THIS DTS IS AUTO GENERATED, DO NOT EDIT
  * 
  * @file eve-esi-types/v2/index.d.ts
- * @summary This file is auto-generated and defines version 3.2.0 of the EVE Online ESI response types.
+ * @summary This file is auto-generated and defines version 3.2.1 of the EVE Online ESI response types.
  */
 import type { TESIResponseOKMap } from "./response-map.d.ts";
 import type { PickPathParameters, InferKeysLen } from "./util.d.ts";
