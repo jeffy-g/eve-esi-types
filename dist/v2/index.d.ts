d51f036873e2c378a2a436e38d6943b90fbb9b43
jeffy-g <hirotom1107@gmail.com>
2025-04-24 20:46:38 +0900

    chore: bump version to v3.2.5

--- dist/v2/index.d.ts
+++ dist/v2/index.d.ts
@@ -9,7 +9,7 @@
  * THIS DTS IS AUTO GENERATED, DO NOT EDIT
  * 
  * @file eve-esi-types/v2/index.d.ts
- * @summary This file is auto-generated and defines version 3.2.4 of the EVE Online ESI response types.
+ * @summary This file is auto-generated and defines version 3.2.5 of the EVE Online ESI response types.
  */
 import type { TESIResponseOKMap } from "./response-map.d.ts";
 import type { PickPathParameters, InferKeysLen, CombineIntersection } from "./util.d.ts";
